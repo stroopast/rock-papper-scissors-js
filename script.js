6ee03891d7bf6e30feb3f6c4760fa9bdd1835b7f
roberto <nanuroberto20@gmail.com>
2025-03-29 15:10:21 +0200

    added buttons, a div containing the buttons, stylized the buttons

--- script.js
+++ script.js
@@ -80,34 +80,81 @@ function playRound(humanchoice, computerChoice)
     }
 }
 
-let humanScore;
-let computerScore;
+// let humanScore;
+// let computerScore;
 
-function playGame()
-{
-    humanScore = 0;
-    computerScore = 0;
-    for(let i = 0 ; i < 5; i++)
-    {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+// function playGame()
+// {
+//     humanScore = 0;
+//     computerScore = 0;
+//     for(let i = 0 ; i < 5; i++)
+//     {
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
 
-    if(humanScore === computerScore)
-    {
-        console.log("Game Draw!");
-    }
-    else if(humanScore <= computerScore)
-    {
-        console.log("You lost the game!");
-    }
-    else if(humanScore >= computerScore)
-    {
-        console.log("You won the game!");
-    }
+//     if(humanScore === computerScore)
+//     {
+//         console.log("Game Draw!");
+//     }
+//     else if(humanScore <= computerScore)
+//     {
+//         console.log("You lost the game!");
+//     }
+//     else if(humanScore >= computerScore)
+//     {
+//         console.log("You won the game!");
+//     }
 
-    console.log(`You scored ${humanScore} points\nComputer scored ${computerScore} points`);
-}
+//     console.log(`You scored ${humanScore} points\nComputer scored ${computerScore} points`);
+// }
 
-playGame();
+// playGame();
+
+
+const rockBtn = document.createElement("button");
+const scissorsBtn = document.createElement("button");
+const paperBtn = document.createElement("button");
+const btnContainer = document.createElement("div");
+
+const docBody = document.querySelector("body");
+
+
+btnContainer.appendChild(rockBtn);
+btnContainer.appendChild(scissorsBtn);
+btnContainer.appendChild(paperBtn);
+
+docBody.appendChild(btnContainer);
+
+btnContainer.classList.toggle("container")
+const container = document.querySelector(".container");
+container.style.display = "flex";
+container.style.justifyContent = "space-between";
+container.style.padding = "100px";
+
+
+rockBtn.textContent = "ROCK";
+scissorsBtn.textContent = "SCISSORS";
+paperBtn.textContent = "PAPER";
+
+///////// Buttons styles /////////
+rockBtn.style.width = "300px";
+rockBtn.style.height = "50px";
+rockBtn.style.color = "red";
+rockBtn.style.fontWeight = "900";
+rockBtn.style.fontSize = "30px";
+
+scissorsBtn.style.width = "300px";
+scissorsBtn.style.height = "50px";
+scissorsBtn.style.color = "blue";
+scissorsBtn.style.fontWeight = "900";
+scissorsBtn.style.fontSize = "30px";
+
+paperBtn.style.width = "300px";
+paperBtn.style.height = "50px";
+paperBtn.style.color = "green";
+paperBtn.style.fontWeight = "900";
+paperBtn.style.fontSize = "30px";
+/////////////////////////////////
+
